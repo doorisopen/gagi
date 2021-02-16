7eb0b1f762e42e70b6c50dd0b5c99f40e78972fc
doorisopen <audauddl2@gmail.com>
2021-02-16 22:51:49 +0900

    feat: 주문 조회 기능 추가
    - 회원의 주문 내역 조회
    - 회원의 주문 상세 내역 조회

--- back/src/main/java/com/gagi/market/order/domain/OrderRepository.java
+++ back/src/main/java/com/gagi/market/order/domain/OrderRepository.java
@@ -1,6 +1,12 @@
 package com.gagi.market.order.domain;
 
+import com.gagi.market.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findOrdersByMember(Member member);
+    Optional<Order> findOrderByOrderIdAndMember(long orderId, Member member);
 }
